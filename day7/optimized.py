5acfb92c6e7ae927949baee9b73d9a0c6511da3f
blank <DLzwAWA8w7yDmQFr@protonmail.com>
2023-12-07 15:03:54 -0800

    da7 optimization

--- day7/optimized.py
+++ day7/optimized.py
@@ -16,13 +16,14 @@ def get_hand_type(hand,part):
         else:
             hand_type[card] = 1
     type_list = hand_type.values()
-    if 5 in type_list:
+    max_count = max(type_list)
+    if max_count == 5:
         return 6
-    elif 4 in type_list:
+    elif max_count == 4:
         if part == 2 and jokers > 0:
             return 6
         return 5
-    elif 3 in type_list:
+    elif max_count == 3:
         if 2 in type_list:
             if part == 2 and jokers > 1:
                 return 6
@@ -30,7 +31,7 @@ def get_hand_type(hand,part):
         elif part == 2 and jokers > 0:
             return 5
         return 3
-    elif 2 in type_list:
+    elif max_count == 2:
         if len(type_list) == 3:
             if part == 2:
                 if jokers == 2:
@@ -48,28 +49,22 @@ def get_hand_type(hand,part):
     return 0
 
 def fixup_hand(hand,cmap):
-    fixed_hand = ""
-    for i in range(5):
-        fixed_hand += cmap[hand[i]]
-    return fixed_hand
+    return ''.join([cmap[x] for x in hand])
 
 def solve(data,part):
     card_data = [[],[],[],[],[],[],[]]
     data = list(map(lambda x: [x[0],int(x[1])],map(lambda x: x.rstrip().split(" "),data)))
     cmap = card_map_joker if part == 2 else card_map
     for line in data:
-        hand = line[0]
-        bet = line[1]
+        hand,bet = line
         hand_type = get_hand_type(hand,part)
-        card_data[hand_type].append([fixup_hand(hand,cmap),hand,bet])
+        card_data[hand_type].append([fixup_hand(hand,cmap),bet])
     winnings = 0
     ranks = 0
     for i in range(7):
-        card_data[i] = sorted(card_data[i])
-        for j in range(len(card_data[i])):
-            rank = j + 1 + ranks
-            winadd = card_data[i][j][2]*rank
-            winnings += winadd
+        card_data[i].sort(key=lambda x:x[0])
+        for j,(_,value) in enumerate(card_data[i]):
+            winnings += value*(j + 1 + ranks)
         ranks += len(card_data[i])
     return winnings
 
